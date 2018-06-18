0bbd3aea88010dec4456c4a975dd20da64091efb
Parryw4456 <40348927+Parryw4456@users.noreply.github.com>
2018-06-18 00:48:59 -0400

    Add files via upload
    P2T1

---
+++ P2T1_salesPrediction_ParryWilliam.py
@@ -0,0 +1,10 @@
+#P2T1
+#SalesPredictions
+#William Parry
+#06/17/18
+#Get the total Sales from the user.
+totalSales = float(input('Enter the projected Sales: '))
+#Calculate the profit as 23 percent of total Sales.
+annualProfit = totalSales * 0.23
+#Display the profit.
+print('The profit is $', format(annualProfit,',.2f'))
